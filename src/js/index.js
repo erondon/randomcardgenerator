9978dee9b491794ac713a2904f5ccf77065887a8
Eleazar Rondon <earc@pop-os.localdomain>
2019-07-22 22:54:15 -0400

    done

--- src/js/index.js
+++ src/js/index.js
@@ -11,17 +11,17 @@ window.onload = function() {
   let randomSym = Math.floor(Math.random() * symbol.length);
   let num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   let randomNum = Math.floor(Math.random() * num.length);
+  let color = "";
 
   if (symbol[randomSym] == "\u2666" || symbol[randomSym] == "\u2665") {
-    document.getElementsByClassName("card-body")[0].style.color = "red";
-    document.getElementsByClassName("card-footer")[0].style.color = "red";
-    document.getElementsByClassName("card-header")[0].style.color = "red";
+    color = "red";
   } else {
-    document.getElementsByClassName("card-body")[0].style.color = "black";
-    document.getElementsByClassName("card-footer")[0].style.color = "black";
-    document.getElementsByClassName("card-header")[0].style.color = "black";
+    color = "black";
   }
 
+  document.getElementsByClassName("card-body")[0].style.color = color;
+  document.getElementsByClassName("card-footer")[0].style.color = color;
+  document.getElementsByClassName("card-header")[0].style.color = color;
   document.getElementsByClassName("card-header")[0].innerHTML =
     "<h1>" + symbol[randomSym] + "</h1>";
   document.getElementsByClassName("card-body")[0].innerHTML =
